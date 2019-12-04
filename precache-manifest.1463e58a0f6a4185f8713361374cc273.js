30a145e73ed70ff53d9090267b1535c9dbdfee20
Karlo Marinovic <36795522+ikaem@users.noreply.github.com>
2019-12-04 19:49:22 +0100

    Updates

---
+++ precache-manifest.1463e58a0f6a4185f8713361374cc273.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153ef17576c2b38a8c47d09714e0aeda",
+    "url": "/swrobot/index.html"
+  },
+  {
+    "revision": "b99dc00a47f847419524",
+    "url": "/swrobot/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "371a3b1b2a59f897aec8",
+    "url": "/swrobot/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b99dc00a47f847419524",
+    "url": "/swrobot/static/js/2.e36bf7a6.chunk.js"
+  },
+  {
+    "revision": "371a3b1b2a59f897aec8",
+    "url": "/swrobot/static/js/main.09097699.chunk.js"
+  },
+  {
+    "revision": "d2c0c93d557febeadb16",
+    "url": "/swrobot/static/js/runtime-main.7d0754e7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/swrobot/static/media/logo.25bf045c.svg"
+  }
+]);
